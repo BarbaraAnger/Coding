5da0c65b4a439c24fa33651438a5505254d012d1
Barbara Anger <barbaraanger@hotmail.com>
2018-08-15 09:12:43 -0300

    Counter update and working

--- Linguagem_C/contadorTempo.c
+++ Linguagem_C/contadorTempo.c
@@ -1,22 +1,23 @@
-/* 
+/*
 * Autora: Bárbara Anger
 * Ano: 2018
 * All rights reserved
 */
 
 #include <stdio.h>
-#define T 86.400
+#define T 86400
+#define H 3600
 void main () {
 int day, hours, min, secs; //declares the variables
 printf("Type hours, minutes and seconds to be calculated:\n");
 scanf("%d %d %d", &hours, &min, &secs);
 
-hours = hours * 60; // turns hours into secs
-min = min * 60; // turns minutes into secs
-secs += min + hours;
+hours *= H; // turns hours into secs
+min *= 60; // turns minutes into secs
+day = secs + min + hours; // sum of hours and minutes(in secs) and also 
+seconds
 
-day = T - secs; // calculates what's left of the day
-
-printf("\tIt's been %d seconds.\n", secs);
-printf("\t%d is left of the day.\n", day);
+printf("\tIt's been %d seconds.\n", day);
+printf("\t%d is left of the day.\n", T-day);
 }
+
