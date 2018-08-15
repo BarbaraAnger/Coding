f2fd172b9c0b1f75adf0c46072075c3ca79c5709
Barbara Anger <barbaraanger@hotmail.com>
2018-08-15 09:15:33 -0300

    Counter f

--- Linguagem_C/contadorTempo.c
+++ Linguagem_C/contadorTempo.c
@@ -7,7 +7,8 @@
 #include <stdio.h>
 #define T 86400
 #define H 3600
-void main () {
+
+int main () {
 int day, hours, min, secs; //declares the variables
 printf("Type hours, minutes and seconds to be calculated:\n");
 scanf("%d %d %d", &hours, &min, &secs);
@@ -19,5 +20,7 @@ seconds
 
 printf("\tIt's been %d seconds.\n", day);
 printf("\t%d is left of the day.\n", T-day);
+
+return 0;
 }
 
