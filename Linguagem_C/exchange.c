17b8c2428b3986bed46c93742c33050cdbe347e5
Bárbara Anger <barbaraanger@hotmail.com>
2018-08-20 17:44:45 -0300

    Now it's good!

--- Linguagem_C/exchange.c
+++ Linguagem_C/exchange.c
@@ -8,12 +8,12 @@
 #include <stdio.h>
 int main(){
     int x, y;
-    printf("Digite dois valores:\n");
+    printf("Typ two values:\n");
     scanf("%d", &x);
     scanf("%d", &y);
     x = x + y;
     y = x - y;
     x = x - y;
-    printf("As variaveis agora sC#o x = %d e y = %d", x, y);
+    printf("The variables are now \n\tx = %d e y = %d\n", x, y);
     return 0;
 }
