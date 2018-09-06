cca88fac290b442a8abdb6b8a1e5091dc69cbd07
Barbara Anger <barbaraanger@hotmail.com>
2018-09-06 15:46:21 -0300

    oi

---
+++ Cpp/Storage.cpp
@@ -0,0 +1,22 @@
+/*
+* Author: Barbara Anger
+* Year: 2018
+*** Copyright. All rights reserved ***
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    int storage[9], x;
+    for (x=0;x<10;x++){
+        storage[x] += 1;
+        if(x==0) {
+            cout<< "Primeiro valor: " << x << endl; //print of the first value
+        } else if(x>0) {
+            cout << x+1 << "º valor: " << storage[x] << endl; //print of the rest
+        }
+        
+    }
+    
+    return 0;
+}
