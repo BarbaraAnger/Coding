09e1941879c4101835e85f8bd9babcc7358c4706
Barbara Anger <barbaraanger@hotmail.com>
2018-10-01 20:04:50 -0300

    Revert numbers

---
+++ C_Language/Q2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+long int N;
+int r=0, s=0;
+printf("Digite um valor:\n");
+scanf("%d", &N);
+while(N>0) {
+    r = N%10;
+    s = (s * 10) + r;
+    N = N/10;
+}
+    printf("revertido: %d\n", s);
+return 0;
+}
