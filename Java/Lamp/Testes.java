353955bfdfc64b75eaa7ff37557659691cea2c65
Bárbara Anger <barbaraanger@hotmail.com>
2019-11-27 15:43:53 -0300

    Update Testes.java

--- Java/Lamp/Testes.java
+++ Java/Lamp/Testes.java
@@ -2,7 +2,7 @@
 @author Barbara Anger
 2018. Copyright. All Rights reserved.
 */
-class Lampada {
+class Testes {
     public static void main(String[] args) {
         Lampada lampada = new Lampada();
         lampada.isOff();
